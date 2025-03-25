172b150e5663dd91293664c10c7eced220f8f241
Isaac Levine <isaacmlevine4@gmail.com>
2025-03-25 13:54:48 -0400

    fix newSubforum page error display and styling

--- client/src/components/main/newSubforum/index.tsx
+++ client/src/components/main/newSubforum/index.tsx
@@ -19,54 +19,55 @@ const NewSubforumPage: React.FC = () => {
     descriptionErr,
     tagsErr,
     rulesErr,
+    error,
     createSubforum,
   } = useNewSubforum();
 
   return (
-    <Form>
-      <Input
-        title='Subforum Title *'
-        hint='Limit title to 100 characters or less'
-        id='formTitleInput'
-        val={title}
-        setState={setTitle}
-        err={titleErr}
-      />
-      <TextArea
-        title='Description *'
-        hint='Describe the purpose and scope of this subforum'
-        id='formDescriptionInput'
-        val={description}
-        setState={setDescription}
-        err={descriptionErr}
-      />
-      <Input
-        title='Tags'
-        hint='Add keywords separated by whitespace'
-        id='formTagInput'
-        val={tags}
-        setState={setTags}
-        err={tagsErr}
-      />
-      <TextArea
-        title='Rules'
-        hint='Add rules for the subforum (one per line)'
-        id='formRulesInput'
-        val={rules}
-        setState={setRules}
-        err={rulesErr}
-      />
-      <div className='btn_indicator_container'>
-        <button
-          className='form_postBtn'
-          onClick={() => {
-            createSubforum();
-          }}>
-          Create Subforum
-        </button>
-        <div className='mandatory_indicator'>* indicates mandatory fields</div>
-      </div>
-    </Form>
+    <div className='new-subforum-container'>
+      <h1>Create New Subforum</h1>
+      {error && <div className='error'>{error}</div>}
+      <Form>
+        <Input
+          title='Subforum Title *'
+          hint='Limit title to 100 characters or less'
+          id='formTitleInput'
+          val={title}
+          setState={setTitle}
+          err={titleErr}
+        />
+        <TextArea
+          title='Description *'
+          hint='Describe the purpose and scope of this subforum'
+          id='formDescriptionInput'
+          val={description}
+          setState={setDescription}
+          err={descriptionErr}
+        />
+        <Input
+          title='Tags'
+          hint='Add keywords separated by whitespace'
+          id='formTagInput'
+          val={tags}
+          setState={setTags}
+          err={tagsErr}
+        />
+        <TextArea
+          title='Rules'
+          hint='Add rules for the subforum (one per line)'
+          id='formRulesInput'
+          val={rules}
+          setState={setRules}
+          err={rulesErr}
+        />
+        <div className='btn_indicator_container'>
+          <button className='form_postBtn' onClick={createSubforum}>
+            Create Subforum
+          </button>
+          <div className='mandatory_indicator'>* indicates mandatory fields</div>
+        </div>
+      </Form>
+    </div>
   );
 };
 
