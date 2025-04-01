c72a94d95ef64268267c81a57bd5e3a96ea6f270
jacob ostapenko <jacobostapenko10@gmail.com>
2025-03-31 23:38:12 -0400

    build

--- client/src/hooks/useSubforumQuestions.ts
+++ client/src/hooks/useSubforumQuestions.ts
@@ -1,6 +1,5 @@
 import { useState, useEffect, useCallback } from 'react';
 import { Question } from '../types';
-import useUserContext from './useUserContext';
 
 const useSubforumQuestions = (subforumId: string | undefined) => {
   const [questions, setQuestions] = useState<Question[]>([]);
